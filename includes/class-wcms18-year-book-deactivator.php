b9419dd1e0ad656f9b8fd3c19e5414b3d1db38d0
Fabian Olsson <fabtasticdesign@live.se>
2019-06-10 09:57:47 +0200

    init

---
+++ includes/class-wcms18-year-book-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       www.fabianolsson.se
+ * @since      1.0.0
+ *
+ * @package    Wcms18_Year_Book
+ * @subpackage Wcms18_Year_Book/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wcms18_Year_Book
+ * @subpackage Wcms18_Year_Book/includes
+ * @author     Fabian Olsson <fab@fab.com>
+ */
+class Wcms18_Year_Book_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
